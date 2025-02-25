7c9bcd9486e32eed9ff4f6e8bff34a346f6716a3
Gauravdevlo <115967551+Gauravdevlo@users.noreply.github.com>
2025-02-26 00:56:32 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,5 +1,11 @@
 import net from "net";
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
+// Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+  const coorelationID = 7;
+  const response = Buffer.from([0,0,0,0,0,0,0,coorelationID]);
+  connection.write(response);
 });
 server.listen(9092, "127.0.0.1");
