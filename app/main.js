d4a8f7bb87d62fc5e7d55ee6589d9fae2a40eacb
Gauravdevlo <115967551+Gauravdevlo@users.noreply.github.com>
2025-02-26 00:58:15 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,13 +1,7 @@
-import net from "net";
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-// Uncomment this block to pass the first stage
+const PORT = 9092;
 const server = net.createServer((connection) => {
-  // Handle connection
-    connection.on("data", (data) => {
-        const request_api_key = data.subarray(0, 2);
-        const request_api_version = data.subarray(2, 4);
-        const correlation_id = data.subarray(4, 12);
-        connection.write(correlation_id);
-    })
+  connection.on("data", (data) => {
+    connection.write(data.subarray(4, 12));
+  });
 });
+server.listen(PORT, "127.0.0.1");
